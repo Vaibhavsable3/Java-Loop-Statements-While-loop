1be68e098b76ca62096480661103b7bee8710197
Vaibhavsable3 <125455641+Vaibhavsable3@users.noreply.github.com>
2025-02-21 19:41:42 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,15 @@
+// Java program to illustrate while loop
+class WhileLoop {
+    public static void main(String args[])
+    {
+        int i = 1;
+
+        // test expression
+        while (i < 6) {
+            System.out.println("Hello Github");
+
+            // update expression
+            i++;
+        }
+    }
+}
